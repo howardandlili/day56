70e3e3e2d1bdb97acefc45c48c89aaf3efc55119
Administrator <1155090@qq.com>
2018-08-31 01:16:02 +0800

    已经完成了ajax不刷新增删学生

--- school/views.py
+++ school/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render,HttpResponse
 from school import models
+import json
 # Create your views here.
 
 
 def students(request):
     stu_list = models.Students.objects.all()
-    return render(request,'students.html',{'stu_list':stu_list})
+    cls_list = models.Classes.objects.all()
+    return render(request,'students.html',{'stu_list':stu_list,'cls_list':cls_list})
 
 
 def stuadd(request):
@@ -30,7 +32,21 @@ def stuadd(request):
     except Exception as e:
         result['status'] = False
         result['errormsg'] = str(e)
-
-    import json
     s = json.dumps(result)
     return HttpResponse(s)
+
+
+def delstu(request):
+    result = {
+        'status': True,
+        'msg': None
+    }
+    try:
+        s_id = request.POST.get('s_id')
+        models.Students.objects.get(id=s_id).delete()
+    except Exception as e:
+        result['status'] = False
+        result['msg'] = str(e)
+    s = json.dumps(result)
+    return HttpResponse(s)
+
