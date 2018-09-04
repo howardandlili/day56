7fd69aa813628a0c095014eb3057f53a4ece468a
Administrator <1155090@qq.com>
2018-09-04 09:48:44 +0800

    开始练习form组件

---
+++ fromdata/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FromdataConfig(AppConfig):
+    name = 'fromdata'
---
+++ fromdata/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, HttpResponse
+from django.forms import fields
+from django import forms
+
+# Create your views here.
+
+class IndexForm(forms.Form):
+    pass
+
+
+
+def index(request):
+    pass
