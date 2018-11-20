5cb109c16fb82d8e0a3007fb1ae5515adeeb3fa8
Administrator <1155090@qq.com>
2018-11-20 18:01:40 +0800

    开始练习form组件

--- fromdata/admin.py
+++ fromdata/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from fromdata import models
+
+
+admin.site.register(models.UserTest)
--- fromdata/models.py
+++ fromdata/models.py
@@ -3,6 +3,27 @@ from django.db import models
 # Create your models here.
 
 
+
+
+
+
+
+class UserTest(models.Model):
+    username = models.CharField(
+        max_length=32,
+        null=True ,#允许为空
+        db_column='user', # 自定义列名
+        db_index=True, #只做加速索引
+        verbose_name='用户名'
+
+    )
+    email = models.EmailField(max_length=32,
+                              null=False,
+                              verbose_name='邮箱')
+
+
 class UserInfo(models.Model):
     username = models.CharField(max_length=32)
     email = models.EmailField(max_length=32)
+
+
