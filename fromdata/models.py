dbfd68ef5c89ad2aba5ca69cd210c84d5c013b5d
Administrator <1155090@qq.com>
2018-11-21 21:25:50 +0800

    model 字段参数

--- fromdata/models.py
+++ fromdata/models.py
@@ -1,19 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
-
-
-
-
-
 
 
 class UserTest(models.Model):
     username = models.CharField(
         max_length=32,
-        null=True ,#允许为空
-        db_column='user', # 自定义列名
-        db_index=True, #只做加速索引
+        null=True,  # 允许为空
+        db_column='user',  # 自定义列名
+        db_index=True,  # 只做加速索引
         verbose_name='用户名'
 
     )
@@ -25,5 +21,3 @@ class UserTest(models.Model):
 class UserInfo(models.Model):
     username = models.CharField(max_length=32)
     email = models.EmailField(max_length=32)
-
-
